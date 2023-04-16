0ac0cc4e446e66a5f25ef7df5f3ee2bd373d98b9
Ethan <757819951@qq.com>
2023-04-16 22:15:43 +0800

    分页

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,11 +19,7 @@ class UsersController extends Controller
             'only' => ['create']
         ]);
     }
-    public function index()
-    {
-        $users = User::all();
-        return view('users.index', compact('users'));
-    }
+
 
 
 
@@ -80,5 +76,12 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user);
+    }
+
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 }

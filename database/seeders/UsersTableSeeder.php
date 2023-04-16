144374dbaa6f28b3e88c7a750a2f29ae9ab10d0f
Ethan <757819951@qq.com>
2023-04-16 22:19:35 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,6 +14,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Ethan';
         $user->email = '7578@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
